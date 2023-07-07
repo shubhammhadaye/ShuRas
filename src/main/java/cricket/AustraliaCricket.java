8bb390da4a6573d9afb109969c6c770af102ca54
shubhammhadaye <shubhammhadaye97@gmail.com>
2023-07-08 00:28:30 +0530

    [shubham] australia team created

---
+++ src/main/java/cricket/AustraliaCricket.java
@@ -0,0 +1,10 @@
+package cricket;
+
+public class AustraliaCricket {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("Australia ");
+	}
+
+}
